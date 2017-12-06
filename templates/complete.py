f9e835e392db6db3cb99e1c4082d6a57b70dd5c0
Somin <sominwadhwa@gmail.com>
2017-12-06 18:52:20 +0530

    setup front-end

---
+++ templates/complete.py
@@ -0,0 +1,13 @@
+{% extends "upload.html" %}
+{% block title %}PreCog Task: Result{% endblock %}
+{% block head %}
+  {{ super() }}
+  <style type="text/css">
+    .important { color: #336699; }
+  </style>
+{% endblock %}
+{% block content %}
+  <h1>Results</h1>
+  <p class="important">
+    Upload Successful
+{% endblock %}
