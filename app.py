87307936db1328a0f6a7277c92eef4e060e2952b
Somin <sominwadhwa@gmail.com>
2017-12-08 02:02:19 +0530

    changed location of storage of uploaded images

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+import time
+
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+import cv2
+import numpy as np
+
+from utils import detect_faces, compile_models, check_ak_or_namo
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+model_ak, model_nm = compile_models()
+haar_face_cascade = cv2.CascadeClassifier(os.path.join(APP_ROOT,'models/data/haarcascade_frontalface_alt.xml'))
+
+@app.route("/")
+def index():
+	return render_template("upload.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+	target = os.path.join(APP_ROOT, 'images/')
+	print (target)
+
+	if not os.path.isdir(target):
+		os.mkdir(target)
+
+	file = request.files['file']
+	print (file)
+	filename = file.filename
+	destination = 'images/'+str(secure_filename(file.filename))
+	file.save(destination)
+	time.sleep(2)
+	print ("Location:" + destination)
+	img = cv2.imread(destination)
+	n_faces, faces_detected_img = detect_faces(haar_face_cascade, img)
+	if n_faces > 0:
+		faces = "YES"
+		if (check_ak_or_namo(destination, model_ak) == True):
+			AK = "YES"
+		else:
+			AK = "NO"
+		if (check_ak_or_namo(destination, model_nm) == True):
+			NM = "YES"
+		else:
+			NM = "NO"
+	else:
+		faces = "NO"
+		AK = "NO"
+		NM = "NO"
+	cv2.imwrite(destination, faces_detected_img)
+	source = '/static/'+file.filename
+	print (source)
+	time.sleep(2)
+	return render_template("upload.html",faces=faces, AK=AK, NM=NM, source=source)
+
---
+++ utils.py
@@ -0,0 +1,40 @@
+import time
+import os
+
+import cv2
+from keras.models import load_model
+import h5py
+import numpy as np
+from keras.preprocessing import image
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+def detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
+    img_copy = colored_img.copy()
+    gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
+    #haar_face_cascade = cv2.CascadeClassifier(os.path.join(APP_ROOT,'models/data/haarcascade_frontalface_alt.xml'))
+    faces = f_cascade.detectMultiScale(gray, scaleFactor=scaleFactor, minNeighbors=5)
+    print (len(faces))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    return len(faces), img_copy
+
+def compile_models():
+	model_nm = load_model("models/nm_cnn.h5")
+	model_ak = load_model("models/ak_cnn.h5")
+	model_nm.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+	model_ak.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+	return model_ak, model_nm
+
+def check_ak_or_namo(path, model):
+	img = image.load_img(path, target_size=(150,150))
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis=0)
+	images = np.vstack([x])
+	classes = model.predict_classes(images, batch_size=10, verbose=0)
+	if (classes[0][0]) == 1:
+		return True
+	else:
+		return False
