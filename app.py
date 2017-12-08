ccd1b43280df9efe12165c09dfcde3c8b7c28ee6
Somin <sominwadhwa@gmail.com>
2017-12-08 11:42:24 +0530

    imporv models

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
 
 @app.route("/upload", methods=['POST'])
 def upload():
-	target = os.path.join(APP_ROOT, 'images/')
+	target = os.path.join(APP_ROOT, 'static/')
 	print (target)
 
 	if not os.path.isdir(target):
@@ -30,26 +30,21 @@ def upload():
 	file = request.files['file']
 	print (file)
 	filename = file.filename
-	destination = 'images/'+str(secure_filename(file.filename))
+	destination = 'static/'+str(secure_filename(file.filename))
 	file.save(destination)
 	time.sleep(2)
 	print ("Location:" + destination)
 	img = cv2.imread(destination)
 	n_faces, faces_detected_img = detect_faces(haar_face_cascade, img)
+	faces="NO"
+	AK="NO"
+	NM="NO"
 	if n_faces > 0:
 		faces = "YES"
 		if (check_ak_or_namo(destination, model_ak) == True):
 			AK = "YES"
-		else:
-			AK = "NO"
 		if (check_ak_or_namo(destination, model_nm) == True):
 			NM = "YES"
-		else:
-			NM = "NO"
-	else:
-		faces = "NO"
-		AK = "NO"
-		NM = "NO"
 	cv2.imwrite(destination, faces_detected_img)
 	source = '/static/'+file.filename
 	print (source)
