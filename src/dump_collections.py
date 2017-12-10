3f98315d0dfa2c8a5d56571e602d51d1d72dd83f
Somin <sominwadhwa@gmail.com>
2017-12-11 02:43:47 +0530

    export_collections

--- src/dump_collections.py
+++ src/dump_collections.py
@@ -1,20 +1,24 @@
-import os
+import os, argparse
 import pymongo
 import gridfs
 
-def dumpToDB(directory, lbl, prt):
+def dumpToDB(directory, lbl, prt, person):
 	conn = pymongo.MongoClient()
-	db = conn.AK_ImgData #Change DB Name accordingly
+	db = conn.data_for_task #Change DB Name accordingly
 	fs = gridfs.GridFS(db)
 	for file in os.listdir(directory):
 		filename=os.fsdecode(file)
 		with open(directory+"/"+filename,'rb') as img:
-			b = fs.put(img, filename=filename, label=lbl, part=prt)
+			b = fs.put(img, filename=filename, label=lbl, part=prt, person=person)
 			print (filename, b)
 
 #Alter the three params accordingly or pass them off as args
-directory = '/Users/sominwadhwa/Desktop/temp/ak/train/positive'
-lbl = 'positive'
-part = 'train'
 
-dumpToDB(directory,lbl,part)
+parser = argparse.ArgumentParser()
+parser.add_argument('-person', type=str, default='Arvind Kejriwal')
+parser.add_argument('-label', type=str, default='positive')
+parser.add_argument('-part', type=str, default='train')
+parser.add_argument('-path', type=str, default='/Users/sominwadhwa/Desktop/temp/ak/train/positive')
+args = parser.parse_args()
+
+dumpToDB(args.path, args.label, args.part, args.person)
--- src/extract_img_example.py
+++ src/extract_img_example.py
@@ -2,11 +2,18 @@ import os
 import pymongo
 import gridfs
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-person', type=str, default='Arvind Kejriwal')
+parser.add_argument('-label', type=str, default='positive')
+parser.add_argument('-part', type=str, default='train')
+
+args = parser.parse_args()
+
 conn = pymongo.MongoClient()
-db = conn.AK_ImgData #Change DB Name accordingly
+db = conn.data_for_task #Change DB Name accordingly
 fs = gridfs.GridFS(db)
 
-for grid_out in fs.find({'label':'positive', 'part':'validation'}):
+for grid_out in fs.find({'label':args.label, 'part':args.part, 'person':args.person}):
     b = grid_out.read()
     outfilename = grid_out.filename
     output= open(outfilename,"wb")
