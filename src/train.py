506d77dba5beb338963ff480e311198972a2e04c
Somin <sominwadhwa@gmail.com>
2017-12-18 18:58:48 +0530

    minor changes

--- src/train.py
+++ src/train.py
@@ -1,9 +1,11 @@
 import h5py #save model
 import pickle as pk
+import argparse
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Activation
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+from keras.optimizers import Adam
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -20,9 +22,9 @@ epochs = args.epochs
 batch_size = args.batch_size
 
 #To evaluate steps per epoch
-train_samples = 800
+train_samples = 1000
 imgw, imgh = 150, 150
-cv_samples = 300
+cv_samples = 400
 
 #Conversion to I/P Format
 if K.image_data_format() == 'channels_first':
@@ -77,7 +79,7 @@ test_datagen = ImageDataGenerator(rescale=1. / 255)
 train_generator = train_datagen.flow_from_directory(train_, target_size=(imgw, imgh), batch_size=batch_size, class_mode='binary')
 validation_generator = test_datagen.flow_from_directory(cv_, target_size=(imgw, imgh), batch_size=batch_size, class_mode='binary')
 
-history = model.fit_generator(train_generator, steps_per_epoch=train_samples // batch_size, epochs=epochs, validation_data=validation_generator, 
+history = model.fit_generator(train_generator, steps_per_epoch=train_samples // batch_size, epochs=epochs, validation_data=validation_generator,
     validation_steps=cv_samples // batch_size)
 
 """ filepath = "weight-improv-{epoch:02d}.hdf5"
